1c96bc7f6f98ada2825197912470ddad17bebb83
Jake Sonnier <jcrsonnier@gmail.com>
2019-11-23 14:22:35 -0600

    init commit. a star works minimally

---
+++ CMPS327-AStar-Game-Starter-Code/Assets/Scripts/Enemy.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// FSM States for the enemy
+public enum EnemyState { STATIC, CHASE, REST, MOVING, DEFAULT };
+
+public enum EnemyBehavior {EnemyBehavior1, EnemyBehavior2, EnemyBehavior3 };
+
+public class Enemy : MonoBehaviour
+{
+    //pathfinding
+    protected PathFinder pathFinder;
+    public GenerateMap mapGenerator;
+    protected Queue<Tile> path;
+    protected GameObject playerGameObject;
+
+    public Tile currentTile;
+    protected Tile targetTile;
+    public Vector3 velocity;
+
+    //properties
+    public float speed = 0.05f;
+    public float visionDistance = 5;
+    public int maxCounter = 5;
+    protected int playerCloseCounter;
+
+    protected EnemyState state = EnemyState.DEFAULT;
+    protected Material material;
+
+    public EnemyBehavior behavior = EnemyBehavior.EnemyBehavior1; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        path = new Queue<Tile>();
+        pathFinder = new PathFinder();
+        playerGameObject = GameObject.FindWithTag("Player");
+        playerCloseCounter = maxCounter;
+        material = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mapGenerator.state == MapState.DESTROYED) return;
+
+        // Stop Moving the enemy if the player has reached the goal
+        if (playerGameObject.GetComponent<Player>().IsGoalReached() || playerGameObject.GetComponent<Player>().IsPlayerDead())
+        {
+            //Debug.Log("Enemy stopped since the player has reached the goal or the player is dead");
+            return;
+        }
+
+        switch(behavior)
+        {
+            case EnemyBehavior.EnemyBehavior1:
+                HandleEnemyBehavior1();
+                break;
+            case EnemyBehavior.EnemyBehavior2:
+                HandleEnemyBehavior2();
+                break;
+            case EnemyBehavior.EnemyBehavior3:
+                HandleEnemyBehavior3();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    public void Reset()
+    {
+        Debug.Log("enemy reset");
+        path.Clear();
+        state = EnemyState.DEFAULT;
+        currentTile = FindWalkableTile();
+        transform.position = currentTile.transform.position;
+    }
+
+    Tile FindWalkableTile()
+    {
+        Tile newTarget = null;
+        int randomIndex = 0;
+        while (newTarget == null || !newTarget.mapTile.Walkable)
+        {
+            randomIndex = (int)(Random.value * mapGenerator.width * mapGenerator.height - 1);
+            newTarget = GameObject.Find("MapGenerator").transform.GetChild(randomIndex).GetComponent<Tile>();
+        }
+        return newTarget;
+    }
+
+    // Dumb Enemy: Keeps Walking in Random direction, Will not chase player
+    private void HandleEnemyBehavior1()
+    {
+        switch (state)
+        {
+            case EnemyState.DEFAULT: // generate random path 
+                material.color = Color.white;
+                if (path.Count <= 0) path = pathFinder.RandomPath(currentTile, 20);
+
+                if (path.Count > 0)
+                {
+                    targetTile = path.Dequeue();
+                    state = EnemyState.MOVING;
+                }
+                break;
+
+            case EnemyState.MOVING:
+                //move
+                velocity = targetTile.gameObject.transform.position - transform.position;
+                transform.position = transform.position + (velocity.normalized * speed);
+                
+                //if target reached
+                if (Vector3.Distance(transform.position, targetTile.gameObject.transform.position) <= speed)
+                {
+                    currentTile = targetTile;
+                    state = EnemyState.DEFAULT;
+                }
+
+                break;
+            default:
+                state = EnemyState.DEFAULT;
+                break;
+        }
+    }
+
+    // TODO: Enemy chases the player when it is nearby
+    private void HandleEnemyBehavior2()
+    {
+        
+    }
+
+    // TODO: Third behavior (Describe what it does)
+    private void HandleEnemyBehavior3()
+    {
+
+    }
+}
---
+++ CMPS327-AStar-Game-Starter-Code/Assets/Scripts/PathFinder.cs
@@ -0,0 +1,288 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using MapGen;
+
+public class Node
+{
+    //data from the node class
+    public Node cameFrom = null; //parent node
+    public double priority = 0; // F value
+    public double costSoFar = 0; // G Value
+    public Tile tile;
+
+    public Node(Tile _tile, double _priority, Node _cameFrom, double _costSoFar)
+    {
+        cameFrom = _cameFrom;
+        priority = _priority; 
+        costSoFar = _costSoFar;
+        tile = _tile;
+    }
+}
+
+public class PathFinder
+{
+    //TODO and done get added as we find a star path
+    List<Node> TODOList = new List<Node>();
+    List<Node> DoneList = new List<Node>();
+    Tile goalTile = null;
+
+    public PathFinder()
+    {
+    }
+
+    public Queue<Tile> FindPathAStar(Tile start, Tile goal)
+    {
+        //setup starting cost
+        double startNodeHCost = HeuristicsDistance(start, goal);
+        Node startNode = new Node(start, startNodeHCost, null, 0);
+
+        //add starting square to todo list
+        TODOList.Add(startNode);
+
+        //first element
+        Node currentNode = TODOList[0];
+
+        //repeat while we havent found goal
+        while(currentNode.tile != goal && TODOList.Count != 0)
+        {
+            currentNode = TODOList[0];
+            TODOList.Remove(currentNode);
+            DoneList.Add(currentNode);
+
+            //look at adjacent squares
+            foreach(Tile adjTile in currentNode.tile.Adjacents)
+            {
+                //Why is passable always true?
+                if(!DoneList.Exists(n => n.tile == adjTile) && adjTile.isPassable)
+                {
+                    if(!TODOList.Exists(n => n.tile == adjTile) && adjTile.isPassable)
+                    {
+                        double adjTileNodeGCost;
+                        double adjTileNodeHCost = HeuristicsDistance(adjTile, goal);
+
+                        if (currentNode.tile.indexX == adjTile.indexX + 1 && currentNode.tile.indexY == adjTile.indexY + 1 || currentNode.tile.indexX == adjTile.indexX + 1 && currentNode.tile.indexY == adjTile.indexY - 1 || currentNode.tile.indexX == adjTile.indexX - 1 && currentNode.tile.indexY == adjTile.indexY + 1 || currentNode.tile.indexX == adjTile.indexX - 1 && currentNode.tile.indexY == adjTile.indexY - 1)
+                        {
+                            //14 for diagonal
+                            adjTileNodeGCost = currentNode.costSoFar + 14;
+                        }
+                        else
+                        {
+                            //10 for straight
+                            adjTileNodeGCost = currentNode.costSoFar + 10;
+                        }
+
+                        double adjTileNodeFCost = adjTileNodeGCost + adjTileNodeHCost;
+                        Node adjTileNode = new Node(adjTile, adjTileNodeFCost, currentNode, adjTileNodeGCost);
+
+                        TODOList.Add(adjTileNode);
+                    }
+                    else
+                    {
+                        //adj tile is in todo list already
+                        Node adjTileNode = TODOList.Find(n => n.tile == adjTile);
+                        if(adjTileNode.priority < currentNode.priority)
+                        {
+                            adjTileNode.cameFrom = currentNode;
+
+                            double adjTileNodeGCost;
+                            double adjTileNodeHCost = HeuristicsDistance(adjTile, goal);
+
+                            if (currentNode.tile.indexX == adjTile.indexX + 1 && currentNode.tile.indexY == adjTile.indexY + 1 || currentNode.tile.indexX == adjTile.indexX + 1 && currentNode.tile.indexY == adjTile.indexY - 1 || currentNode.tile.indexX == adjTile.indexX - 1 && currentNode.tile.indexY == adjTile.indexY + 1 || currentNode.tile.indexX == adjTile.indexX - 1 && currentNode.tile.indexY == adjTile.indexY - 1)
+                            {
+                                adjTileNodeGCost = adjTileNode.cameFrom.costSoFar + 14;
+                            }
+                            else
+                            {
+                                adjTileNodeGCost = adjTileNode.cameFrom.costSoFar + 10;
+                            }
+
+                            double adjTileNodeFCost = adjTileNodeGCost + adjTileNodeHCost;
+
+                            adjTileNode.priority = adjTileNodeFCost;
+                            adjTileNode.costSoFar = adjTileNodeGCost;
+
+                            TODOList.Sort((x, y) => x.priority.CompareTo(y.priority));
+                        }
+                    }
+                }
+            }
+        }
+
+        Node targetNode = currentNode;
+        Queue<Tile> finalPath = new Queue<Tile>();
+        while (targetNode != null)
+        {
+            Debug.Log(targetNode.ToString());
+            finalPath.Enqueue(targetNode.tile);
+            targetNode = targetNode.cameFrom;
+        }
+
+        return finalPath; // returns path
+    }
+
+    // TODO: Find the path based on A-Star Algorithm
+    // In this case avoid a path passing near an enemy tile
+    //only can pathfind, no enemy evasion yet
+    public Queue<Tile> FindPathAStarEvadeEnemy(Tile start, Tile goal)
+    {
+        double startNodeHCost = HeuristicsDistance(start, goal);
+        Node startNode = new Node(start, startNodeHCost, null, 0);
+
+        //add starting square to todo list
+        TODOList.Add(startNode);
+
+        Node currentNode = TODOList[0];
+
+        while (currentNode.tile != goal && TODOList.Count != 0)
+        {
+            currentNode = TODOList[0];
+            TODOList.Remove(currentNode);
+            DoneList.Add(currentNode);
+
+            foreach (Tile adjTile in currentNode.tile.Adjacents)
+            {
+                if (!DoneList.Exists(n => n.tile == adjTile) && !adjTile.isPassable)
+                {
+                    if (!TODOList.Exists(n => n.tile == adjTile) && !adjTile.isPassable)
+                    {
+                        double adjTileNodeGCost;
+                        double adjTileNodeHCost = HeuristicsDistance(adjTile, goal);
+
+                        if (currentNode.tile.indexX == adjTile.indexX + 1 && currentNode.tile.indexY == adjTile.indexY + 1 || currentNode.tile.indexX == adjTile.indexX + 1 && currentNode.tile.indexY == adjTile.indexY - 1 || currentNode.tile.indexX == adjTile.indexX - 1 && currentNode.tile.indexY == adjTile.indexY + 1 || currentNode.tile.indexX == adjTile.indexX - 1 && currentNode.tile.indexY == adjTile.indexY - 1)
+                        {
+                            adjTileNodeGCost = currentNode.costSoFar + 14;
+                        }
+                        else
+                        {
+                            adjTileNodeGCost = currentNode.costSoFar + 10;
+                        }
+
+                        double adjTileNodeFCost = adjTileNodeGCost + adjTileNodeHCost;
+                        Node adjTileNode = new Node(adjTile, adjTileNodeFCost, currentNode, adjTileNodeGCost);
+
+                        TODOList.Add(adjTileNode);
+                    }
+                    else
+                    {
+                        //adj tile is in todo list
+                        Node adjTileNode = TODOList.Find(n => n.tile == adjTile);
+                        if (adjTileNode.priority < currentNode.priority)
+                        {
+                            //path to adj tile is better than current node
+                            adjTileNode.cameFrom = currentNode;
+
+                            double adjTileNodeGCost;
+                            double adjTileNodeHCost = HeuristicsDistance(adjTile, goal);
+
+                            if (currentNode.tile.indexX == adjTile.indexX + 1 && currentNode.tile.indexY == adjTile.indexY + 1 || currentNode.tile.indexX == adjTile.indexX + 1 && currentNode.tile.indexY == adjTile.indexY - 1 || currentNode.tile.indexX == adjTile.indexX - 1 && currentNode.tile.indexY == adjTile.indexY + 1 || currentNode.tile.indexX == adjTile.indexX - 1 && currentNode.tile.indexY == adjTile.indexY - 1)
+                            {
+                                adjTileNodeGCost = adjTileNode.cameFrom.costSoFar + 14;
+                            }
+                            else
+                            {
+                                adjTileNodeGCost = adjTileNode.cameFrom.costSoFar + 10;
+                            }
+
+                            double adjTileNodeFCost = adjTileNodeGCost + adjTileNodeHCost;
+
+                            adjTileNode.priority = adjTileNodeFCost;
+                            adjTileNode.costSoFar = adjTileNodeGCost;
+
+                            TODOList.Sort((x, y) => x.priority.CompareTo(y.priority));
+                        }
+                    }
+                }
+            }
+        }
+
+        Node targetNode = currentNode;
+        Queue<Tile> path = new Queue<Tile>();
+        while (targetNode != null)
+        {
+            finalPath.Enqueue(targetNode.tile);
+            targetNode = targetNode.cameFrom;
+        }
+
+        //the a star path, doesnt seem to follow tiles
+        return path;
+    }
+
+    // Manhattan Distance with horizontal/vertical cost of 10
+    double HeuristicsDistance(Tile currentTile, Tile goalTile)
+    {
+        int xdist = Math.Abs(goalTile.indexX - currentTile.indexX);
+        int ydist = Math.Abs(goalTile.indexY - currentTile.indexY);
+        // Assuming cost to move horizontally and vertically is 10
+        //return manhattan distance
+        return (xdist * 10 + ydist * 10);
+    }
+
+    // Retrace path from a given Node back to the start Node
+    Queue<Tile> RetracePath(Node node)
+    {
+        List<Tile> tileList = new List<Tile>();
+        Node nodeIterator = node;
+        while (nodeIterator.cameFrom != null)
+        {
+            tileList.Insert(0, nodeIterator.tile);
+            nodeIterator = nodeIterator.cameFrom;
+        }
+        return new Queue<Tile>(tileList);
+    }
+
+    // Generate a Random Path. Used for enemies
+    public Queue<Tile> RandomPath(Tile start, int stepNumber)
+    {
+        List<Tile> tileList = new List<Tile>();
+        Tile currentTile = start;
+        for (int i = 0; i < stepNumber; i++)
+        {
+            Tile nextTile;
+            //find random adjacent tile different from last one if there's more than one choice
+            if (currentTile.Adjacents.Count < 0)
+            {
+                break;
+            }
+            else if (currentTile.Adjacents.Count == 1)
+            {
+                nextTile = currentTile.Adjacents[0];
+            }
+            else
+            {
+                nextTile = null;
+                List<Tile> adjacentList = new List<Tile>(currentTile.Adjacents);
+                ShuffleTiles<Tile>(adjacentList);
+                if (tileList.Count <= 0) nextTile = adjacentList[0];
+                else
+                {
+                    foreach (Tile tile in adjacentList)
+                    {
+                        if (tile != tileList[tileList.Count - 1])
+                        {
+                            nextTile = tile;
+                            break;
+                        }
+                    }
+                }
+            }
+            tileList.Add(currentTile);
+            currentTile = nextTile;
+        }
+        return new Queue<Tile>(tileList);
+    }
+
+    private void ShuffleTiles<T>(List<T> list)
+    {
+        // Knuth shuffle algorithm :: 
+        // courtesy of Wikipedia :) -> https://forum.unity.com/threads/randomize-array-in-c.86871/
+        for (int t = 0; t < list.Count; t++)
+        {
+            T tmp = list[t];
+            int r = UnityEngine.Random.Range(t, list.Count);
+            list[t] = list[r];
+            list[r] = tmp;
+        }
+    }
+}
